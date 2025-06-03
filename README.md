73bd2ae3cfb071f97f1010e920def8b825835b57
htfab <mpw@htamas.net>
2025-06-03 03:48:42 +0200

    add files to version control

---
+++ README.md
@@ -0,0 +1,5 @@
+# align.py
+
+Aligns an image to another reference image based on a set of control points. Uses Hugin to optimize positioning (yaw, pitch, roll, translate) and lens distortion (barrel, pincushion, wavy, shift, shear, perspective).
+
+
---
+++ align.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import subprocess
+
+cfg = json.load(open(sys.argv[1] if len(sys.argv) > 1 else "config.json"))
+
+anchor_input = cfg["anchor"]["input"]
+anchor_cp = cfg["anchor"]["control_points"]
+remap_input = cfg["remap"]["input"]
+remap_output = cfg["remap"]["output"]
+remap_cp = cfg["remap"]["control_points"]
+output_format = {"tif": "TIFF", "png": "PNG", "jpg": "JPEG"}[remap_output.split(".")[-1]]
+
+subprocess.run(["pto_gen", "-o", "project.pto", "-p", "0", "-f", "10", "-s", "1", anchor_input, remap_input], check=True)
+
+with open("project.pto") as f:
+    imgline = next(l for l in f if l.startswith("i"))
+    i, w, h, *_ = imgline.split()
+    assert i == "i" and w.startswith("w") and h.startswith("h")
+    width = int(w.removeprefix("w"))
+    height = int(h.removeprefix("h"))
+
+subprocess.run(["pto_var", "-o", "project.pto", "--anchor=0", "--opt=y1,p1,r1,TrX1,TrY1,TrZ1,Tpy1,Tpp1,v1,a1,b1,c1,d1,e1,g1,t1", "project.pto"], check=True)
+
+with open("project.pto", "a") as f:
+    for (x0, y0), (x1, y1) in zip(anchor_cp, remap_cp):
+        print(f"c n0 N1 x{x0} y{y0} X{x1} Y{y1} t0", file=f)
+
+subprocess.run(["autooptimiser", "-o", "project.pto", "-n", "project.pto"], check=True)
+
+subprocess.run(["pano_modify", "-o", "project.pto", "-p", "0", "--fov=10", f"--canvas={width}x{height}", "--output-type=REMAPORIG", "project.pto"], check=True)
+
+subprocess.run(["nona", "-o", remap_output, "-i", "1", "-m", output_format, "project.pto"], check=True)
+
